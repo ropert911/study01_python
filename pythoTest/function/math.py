ce596dd4e4f5aeb989e50d6b5404e91bc6d4fc6d
ropert911 <ropert911@163.com>
2018-08-24 06:56:15 +0800

    添加实例

---
+++ pythoTest/function/math.py
@@ -0,0 +1,11 @@
+import math
+
+sum = sum([1, 2, 3])
+print("sum={}".format(sum))
+max = max([1, 2, 3])
+print("max={}".format(max))
+min = min([1, 2, 3])
+print("min={}".format(min))
+
+#向上取整
+print("math.ceil(1.5) = {}".format(math.ceil(1.5)))
